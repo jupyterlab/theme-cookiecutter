8ecdf3b675dfb0d34bb2f9acf081c14ec6c1a03a
telamonian <mklein@jhu.edu>
2019-02-18 07:00:41 -0500

    The CI test will now run JupyterLab with the new theme enabled

--- ci_script.sh
+++ ci_script.sh
@@ -6,8 +6,25 @@
 set -ex
 set -o pipefail
 
-# Make sure we can create, install, and run the default theme extension created by the cookiecutter
+JL_SETTINGS=./.jupyter/lab/user-settings
+THEME_SETTINGS=${JL_SET_DIR}/@jupyterlab/apputils-extension/themes.jupyterlab-settings
+
+# Create a theme extension using the cookiecutter default inputs
 cookiecutter . --no-input
+
+# install theme
 jupyter labextension install ./mytheme
-python -m jupyterlab.browser_check
+
+# enable the theme in user-settings
+mkdir -p $(dirname $THEME_SET)
+cat > THEME_SETTINGS << EOF
+{
+    "theme": "mytheme"
+}
+EOF
+
+# print out a confirmation that the extension is installed
 jupyter labextension list
+
+# run a test of the main JupyterLab app with the theme enabled
+JUPYTERLAB_SETTINGS_DIR=$JL_SETTINGS python -m jupyterlab.browser_check
