68fc11d11f0856eff0efcd427506107b29851c47
telamonian <mklein@jhu.edu>
2019-02-18 07:26:06 -0500

    more bugfix

--- ci_script.sh
+++ ci_script.sh
@@ -7,8 +7,8 @@ set -ex
 set -o pipefail
 
 # set a custom user-settings directory
-export JUPYTERLAB_SETTINGS_DIR=./.jupyter/lab/user-settings
-THEME_SETTINGS=${JUPYTERLAB_SETTINGS_DIR}/@jupyterlab/apputils-extension/themes.jupyterlab-settings
+export JUPYTERLAB_SETTINGS_DIR="./.jupyter/lab/user-settings"
+THEME_SETTINGS="${JUPYTERLAB_SETTINGS_DIR}/@jupyterlab/apputils-extension/themes.jupyterlab-settings"
 
 # Create a theme extension using the cookiecutter default inputs
 cookiecutter . --no-input
@@ -18,11 +18,7 @@ jupyter labextension install ./mytheme
 
 # enable the theme in user-settings
 mkdir -p $(dirname $THEME_SETTINGS)
-cat > THEME_SETTINGS << EOF
-{
-    "theme": "mytheme"
-}
-EOF
+printf "%s\n" "{" "    \"theme\": \"mytheme\"" "}" > $THEME_SETTINGS
 
 # print out a confirmation that the extension is installed
 jupyter labextension list
