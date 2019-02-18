0807063e99db4975a4f403144399156b4be7cb61
telamonian <mklein@jhu.edu>
2019-02-18 11:06:55 -0500

    Changes to class names in 1.0 were breaking the build, fixed them

--- {{cookiecutter.extension_name}}/src/index.ts
+++ {{cookiecutter.extension_name}}/src/index.ts
@@ -1,29 +1,26 @@
 {%- set extension_id = [cookiecutter.org_name, cookiecutter.extension_name]|join('/') if cookiecutter.org_name else cookiecutter.extension_name -%}
 
 import {
-  JupyterLab, JupyterLabPlugin
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
-import {
-  IThemeManager
-} from '@jupyterlab/apputils';
+import { IThemeManager } from '@jupyterlab/apputils';
 
 /**
  * A plugin for {{ extension_id }}
  */
-const plugin: JupyterLabPlugin<void> = {
+const plugin: JupyterFrontEndPlugin<void> = {
   id: '{{ extension_id }}:plugin',
   requires: [IThemeManager],
-  activate: function(app: JupyterLab, manager: IThemeManager) {
+  activate: function(app: JupyterFrontEnd, manager: IThemeManager) {
+    const style = '{{ extension_id }}/index.css';
+
     manager.register({
       name: '{{ cookiecutter.extension_name }}',
       isLight: true,
-      load: function() {
-        return manager.loadCSS('{{ extension_id }}/index.css');
-      },
-      unload: function() {
-        return Promise.resolve(void 0);
-      }
+      load: () => manager.loadCSS(style),
+      unload: () => Promise.resolve(undefined)
     });
   },
   autoStart: true
